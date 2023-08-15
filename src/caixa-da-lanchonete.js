eab85a0e41248f481cc6b1719a3a5bfbd8bf2ef8
Giovane Roxo <giovaneroxo@gmail.com>
2023-08-15 19:18:59 -0300

    finalizando regra de negocio

--- src/caixa-da-lanchonete.js
+++ src/caixa-da-lanchonete.js
@@ -5,13 +5,20 @@ class CaixaDaLanchonete {
     calcularValorDaCompra(metodoDePagamento, itens) {
 
     let carrinho = [];
-    let valorTotal = 0.00;
-    let ValorDaCompra = 0.00;
+    let valor = 0.00;
+    let ValorTotal = 0.00;
     let itensDaLanchonete = new ItensDaLanchonete();
+    let qtdCafe;
+    let qtdSanduiche;
 
     for(let i = 0; i < itens.length; i++){
         let item = itens[i].split(",");
         carrinho.push(item.map((item) => item.trim()));
+        if(carrinho[i][0] == "cafe"){
+            qtdCafe = carrinho[i][1];
+        }else if(carrinho[i][0] == "sanduiche"){
+            qtdSanduiche = carrinho[i][1];
+        }
     }
 
     if(itens.length == 0){
@@ -20,20 +27,21 @@ class CaixaDaLanchonete {
         return "Forma de pagamento inválida!";
     }
 
-
-    
-
     for(let i = 0; i < carrinho.length; i++){
         if(itensDaLanchonete.validarItem(carrinho[i][0]) == false){
             return "Item inválido!";
-        }else if(itensDaLanchonete.validarItem(carrinho[i][0]) == true && carrinho[i][1] <= 0){
+        }else if(carrinho[i][1] <= 0){
             return "Quantidade inválida!"
-        }else if(itensDaLanchonete.getItens().includes(carrinho[i][0]) == true && carrinho[i][1] > 0){
-            valorTotal += itensDaLanchonete.getValorItem(carrinho[i][0], carrinho[i][1]);
+        }else if(carrinho[i][0] === "chantily" && qtdCafe < carrinho[i][1] ^ qtdCafe === undefined){
+            return "Item extra não pode ser pedido sem o principal";
+        }else if(carrinho[i][0] === "queijo" && qtdSanduiche < carrinho[i][1] ^ qtdSanduiche === undefined){
+            return "Item extra não pode ser pedido sem o principal";
+        }else{
+            valor += itensDaLanchonete.getValorItem(carrinho[i][0], carrinho[i][1]);
         }    
     }
-    ValorDaCompra =  itensDaLanchonete.getValorTotalComDesconto(valorTotal, metodoDePagamento);
-    return "R$ " + ValorDaCompra.toFixed(2).replace(".", ",");
+    ValorTotal =  itensDaLanchonete.getValorTotalComDesconto(valor, metodoDePagamento);
+    return "R$ " + ValorTotal.toFixed(2).replace(".", ",");
     }
 }
 
